202be98a1e9c9b537a438ef322128ce886f131b9
Husnain-pro <54207663+Husnain-pro@users.noreply.github.com>
2021-08-06 18:08:12 +0500

    refactor code

--- pages/country/[name].js
+++ pages/country/[name].js
@@ -1,7 +1,7 @@
 import { ArrowLeft } from "components/Icons";
 import Navbar from "components/navbar/Navbar";
 import { CountryContext } from "context/CountryContext";
-import UseCountry from "hooks/useCountry";
+import UseCountry from "hooks/UseCountry";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
