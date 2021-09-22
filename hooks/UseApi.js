aa43af3f07ee5fcc4d0705566bef8b4461c57b61
Husnain-pro <54207663+Husnain-pro@users.noreply.github.com>
2021-09-22 23:37:35 +0500

    updata header

--- hooks/UseApi.js
+++ hooks/UseApi.js
@@ -5,7 +5,14 @@ const UseApi = () => {
   return {
     get: async (endpoint) => {
       try {
-        const { data } = await axios.get(`${url}${endpoint}`);
+        // const { data } = await axios.get(`${url}${endpoint}`);
+        const { data } = await axios.get({
+          baseUrl: `${url}${endpoint}`,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          },
+        });
         return data;
       } catch (error) {
         console.log(error);
